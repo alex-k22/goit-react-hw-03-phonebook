93ebc4d609a8c5a986c1ff1eac7e35baaf049058
alex-k22 <114990357+alex-k22@users.noreply.github.com>
2023-06-18 21:44:04 +0300

    reset inputs after submit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -35,6 +35,7 @@ class Form extends Component {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
+          value={this.state.name}
           onChange={this.handleInputChange}
         />
         <label htmlFor={this.numberInputId}>Number</label>
@@ -46,6 +47,7 @@ class Form extends Component {
           pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
           title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
           required
+          value={this.state.number}
           onChange={this.handleInputChange}
         />
         <button type="submit" className={css.button}>
